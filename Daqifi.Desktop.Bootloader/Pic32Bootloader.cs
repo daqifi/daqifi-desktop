96f2828e40bbe8d35d06ce06011486db5b8e782a
tylerkron <tylerkron@gmail.com>
2018-02-21 14:58:14 -0700

    DA-33 Firmware Uploader now preserves memory locations from 0x1D040000 to 0x1D057FFF

--- Daqifi.Desktop.Bootloader/Pic32Bootloader.cs
+++ Daqifi.Desktop.Bootloader/Pic32Bootloader.cs
@@ -18,6 +18,10 @@ namespace Daqifi.Desktop.Bootloader
         private string _version;
         private bool _attached;
         private bool _disposed;
+        private ushort _baseAddress;
+        private uint _beginProtectedAddress = 0x1D040000;
+        private uint _endProtectedAddress = 0x1D057FFF;
+
         #endregion
 
         #region Properties
@@ -139,10 +143,40 @@ namespace Daqifi.Desktop.Bootloader
                     hexLine.Add(Convert.ToByte(int.Parse(new string(asciiCharacters), NumberStyles.HexNumber)));
                 }
 
+                // Skip if this is in a protected memory range
+                if (IsProtectedHexRecord(hexLine.ToArray())) continue;
+
                 hexRecords.Add(hexLine.ToArray());
             }
 
             return hexRecords;
+        }
+
+        private bool IsProtectedHexRecord(byte[] hexRecord)
+        {
+            var offsetAddressArray = hexRecord.Skip(1).Take(2).ToArray();
+            var recordType = hexRecord[3];
+            var dataArray = hexRecord.Skip(4).Take(hexRecord.Length - 5).ToArray();
+
+            // Set Base Address
+            if (recordType == 0x04)
+            {
+                if (BitConverter.IsLittleEndian) Array.Reverse(dataArray);
+                _baseAddress = BitConverter.ToUInt16(dataArray, 0);
+            }
+            // Data
+            else if (recordType == 0x00)
+            {
+                if (BitConverter.IsLittleEndian) Array.Reverse(offsetAddressArray);
+                var offsetAddress = BitConverter.ToUInt16(offsetAddressArray, 0);
+                var hexRecordAddress = (_baseAddress << 16) | offsetAddress;
+
+                if (hexRecordAddress >= _beginProtectedAddress && hexRecordAddress <= _endProtectedAddress)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public static byte[] Combine(params byte[][] arrays)
