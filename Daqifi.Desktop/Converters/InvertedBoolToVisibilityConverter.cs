3db293e168c880ef33a0eedd655c3ec0d0333a4d
Tyler Kron <tylerkron@gmail.com>
2025-04-01 22:34:36 -0600

    fix: disable firmware update option on WiFi (#136)

---
+++ Daqifi.Desktop/Converters/InvertedBoolToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Daqifi.Desktop.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class InvertedBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool booleanValue)
+            {
+                return booleanValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Collapsed; // Default to collapsed if input is not bool
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibilityValue)
+            {
+                return visibilityValue == Visibility.Collapsed;
+            }
+            return false; // Default or throw exception based on requirements
+        }
+    }
+} 
